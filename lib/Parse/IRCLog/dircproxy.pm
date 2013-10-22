60aab5fd491736942cceea6f986938f6f51b35fb
Richard Lewis <richard@rjlewis.me.uk>
2013-10-22 01:03:28 +0100

    Fixed action event match groups

--- lib/Parse/IRCLog/dircproxy.pm
+++ lib/Parse/IRCLog/dircproxy.pm
@@ -10,7 +10,7 @@ sub patterns {
     return $_[0]{patterns} if ref $_[0] and defined $_[0]{patterns};
 
     {msg    => qr/^@([0-9]+)\s+<([+%@])?([^!]+)![^>]+>(\s)(.+)/,
-     action => qr/^@([0-9]+)\s+\[([+%@])?([^!]+)![^\]]+\](\s)ACTION\s(.+)/};
+     action => qr/^@([0-9]+)\s+\[([+%@])?([^!]+)![^\]]+\]\sACTION\s(.+)/};
 }
 
 1;
